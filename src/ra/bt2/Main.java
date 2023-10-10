aa3ec952bb66909e4ded464d54b148436ee301c7
Nguyễn Xuân Thành <nxt1012@gmail.com>
2023-10-10 11:55:09 +0700

    fix bug

--- src/ra/bt2/Main.java
+++ src/ra/bt2/Main.java
@@ -11,13 +11,24 @@ public class Main {
         int[] arr = new int[arrLength];
         if (arr.length == 0) {
             throw new NullPointerException("Mảng rỗng");
-        }
-        int max = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[0] > max) {
-                max = arr[0];
+        } else {
+            for (int i = 0; i < arr.length; i++) {
+                System.out.println("Nhập vào số thứ " + (i + 1) + ": ");
+                arr[i] = Integer.parseInt(sc.nextLine());
             }
+            int max = searchMax(arr);
+            System.out.println("Số lớn nhất trong mảng là: " + max);
         }
 
+    }
+
+    private static int searchMax(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
     }
 }
